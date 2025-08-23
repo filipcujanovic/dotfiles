631fe49145ac5b92af679f6550dfa283f8ddae73
filipcujanovic <29860740+filipcujanovic@users.noreply.github.com>
2025-08-23 15:39:15 +0200

    feat(dashboard.nvim): new config

--- install.sh
+++ install.sh
@@ -11,22 +11,21 @@ ln -s -f ~/dotfiles/.config/btop ~/.config/
 ln -s -f ~/dotfiles/.config/delta ~/.config/
 ln -s -f ~/dotfiles/.config/figlet ~/.config/
 ln -s -f ~/dotfiles/.config/ghostty ~/.config/
-ln -s -f ~/dotfiles/.config/nvim/ ~/.config/
 ln -s -f ~/dotfiles/.config/kitty/ ~/.config/
-
+ln -s -f ~/dotfiles/.config/nvim/ ~/.config/
 ln -s -f ~/dotfiles/.config/starship ~/.config/
 ln -s -f ~/dotfiles/.config/tmux/ ~/.config/
 ln -s -f ~/dotfiles/.config/wezterm ~/.config/
 
 ln -s -f ~/dotfiles/.config/git/.gitconfig ~/.gitconfig
 ln -s -f ~/dotfiles/.config/gitmux/.gitmux.conf ~/.gitmux.conf
+ln -s -f ~/dotfiles/.config/mycli/.myclirc ~/.myclirc
 ln -s -f ~/dotfiles/.config/tmux/.tmux.conf ~/.tmux.conf
 ln -s -f ~/dotfiles/.config/tmuxifier/* ~/.tmuxifier/layouts/
-ln -s -f ~/dotfiles/.config/mycli/.myclirc ~/.myclirc
+ln -s -f ~/dotfiles/.config/visidata/.visidatarc ~/.visidatarc
+ln -s -f ~/dotfiles/.config/yazi/theme.toml ~/.config/yazi/theme.toml
 ln -s -f ~/dotfiles/.config/zsh/.zshenv ~/.zshenv
 ln -s -f ~/dotfiles/.config/zsh/.zshrc ~/.zshrc
-ln -s -f ~/dotfiles/.config/yazi/theme.toml ~/.config/yazi/theme.toml
-ln -s -f ~/dotfiles/.config/visidata/.visidatarc ~/.visidatarc
 
 if [[ "$os_name" == "Darwin" ]]; then
 	ln -s -f ~/dotfiles/.config/aerospace ~/.config/
