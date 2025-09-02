e3a5feb96a3ed5a7d72c49757825d41f09eb7a4a
filipcujanovic <29860740+filipcujanovic@users.noreply.github.com>
2025-09-02 14:44:59 +0200

    update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # symlink .config dir
-os=$(uname -S)
+os=$(uname -s)
 
 ln -s -f ~/dotfiles/.config/shared/* ~/.config/
 
@@ -12,25 +12,25 @@ ln -s -f ~/dotfiles/.config/shared/tmux/.tmux.conf ~/.tmux.conf
 ln -s -f ~/dotfiles/.config/shared/visidata/.visidatarc ~/.visidatarc
 ln -s -f ~/dotfiles/.config/shared/zsh/.zshenv ~/.zshenv
 ln -s -f ~/dotfiles/.config/shared/zsh/.zshrc ~/.zshrc
-ln -s -f ~/dotfiles/bin/shared* ~/bin/
+ln -s -f ~/dotfiles/bin/shared/* ~/bin/
 
 if [[ os == "Darwin" ]]; then
     #brew bundle --file=~/dotfiles/Brewfile
-    ln -s -f ~/dotfiles/bin/macos* ~/bin/
+    ln -s -f ~/dotfiles/bin/macos/* ~/bin/
     ln -s -f ~/dotfiles/.config/macos/* ~/.config/
     ln -s -f ~/dotfiles/.config/macos/zsh/.zshenv ~/.zshenv
     ln -s -f ~/dotfiles/.config/macos/zsh/.zshrc ~/.zshrc
-    ln -s -f ~/dotfiles/.config/shared/lazygit/config.yaml ~/Library/Application\ Support/lazygit/config.yml
+    ln -s -f ~/dotfiles/.config/shared/lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
     ln -s -f ~/dotfiles/.config/macos/hammerspoon/init.lua ~/.hammerspoon/init.lua
     curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/latest/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
-	ln -s -f ~/dotfiles/.config/aerospace ~/.config/
-	ln -s -f ~/dotfiles/.config/borders ~/.config/
-	ln -s -f ~/dotfiles/.config/hammerspoon/init.lua ~/.hammerspoon/init.lua
-	ln -s -f ~/dotfiles/.config/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
-	ln -s -f ~/dotfiles/.config/lazygit/config.yaml ~/Library/Application\ Support/lazygit/config.yml
-	ln -s -f ~/dotfiles/.config/lazysql/config.yaml ~/Library/Application\ Support/lazysql/config.toml
-	ln -s -f ~/dotfiles/.config/sketchybar ~/.config/
-	ln -s -f ~/dotfiles/.config/skhd ~/.config/
+    ln -s -f ~/dotfiles/.config/aerospace ~/.config/
+    ln -s -f ~/dotfiles/.config/borders ~/.config/
+    ln -s -f ~/dotfiles/.config/hammerspoon/init.lua ~/.hammerspoon/init.lua
+    ln -s -f ~/dotfiles/.config/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
+    ln -s -f ~/dotfiles/.config/lazysql/config.yaml ~/Library/Application\ Support/lazysql/config.toml
+    ln -s -f ~/dotfiles/.config/sketchybar ~/.config/
+    ln -s -f ~/dotfiles/.config/skhd ~/.config/
 else
     ln -s -f ~/dotfiles/.config/linux/* ~/.config/
+    ln -s -f ~/dotfiles/bin/linux/* ~/bin/
 fi
