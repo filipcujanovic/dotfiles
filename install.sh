bf043f7db68abee87cd2eb8e936c591dfbf9b76c
filipcujanovic <29860740+filipcujanovic@users.noreply.github.com>
2025-09-05 21:41:50 +0200

    fix: fix bugs for macos install script

--- install.sh
+++ install.sh
@@ -14,22 +14,15 @@ ln -s -f ~/projects/dotfiles/.config/shared/zsh/.zshenv ~/.zshenv
 ln -s -f ~/projects/dotfiles/.config/shared/zsh/.zshrc ~/.zshrc
 ln -s -f ~/projects/dotfiles/bin/shared/* ~/bin/
 
-if [[ os == "Darwin" ]]; then
+if [[ $os == "Darwin" ]]; then
+    defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
     #brew bundle --file=~/projects/dotfiles/Brewfile
     ln -s -f ~/projects/dotfiles/bin/macos/* ~/bin/
     ln -s -f ~/projects/dotfiles/.config/macos/* ~/.config/
-    ln -s -f ~/projects/dotfiles/.config/macos/zsh/.zshenv ~/.zshenv
-    ln -s -f ~/projects/dotfiles/.config/macos/zsh/.zshrc ~/.zshrc
     ln -s -f ~/projects/dotfiles/.config/shared/lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
-    ln -s -f ~/projects/dotfiles/.config/macos/hammerspoon/init.lua ~/.hammerspoon/init.lua
-    curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/latest/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
-    ln -s -f ~/projects/dotfiles/.config/aerospace ~/.config/
-    ln -s -f ~/projects/dotfiles/.config/borders ~/.config/
-    ln -s -f ~/projects/dotfiles/.config/hammerspoon/init.lua ~/.hammerspoon/init.lua
-    ln -s -f ~/projects/dotfiles/.config/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
-    ln -s -f ~/projects/dotfiles/.config/lazysql/config.yaml ~/Library/Application\ Support/lazysql/config.toml
-    ln -s -f ~/projects/dotfiles/.config/sketchybar ~/.config/
-    ln -s -f ~/projects/dotfiles/.config/skhd ~/.config/
+    #ln -s -f ~/projects/dotfiles/.config/macos/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
+    #ln -s -f ~/projects/dotfiles/.config/macos/lazysql/config.yaml ~/Library/Application\ Support/lazysql/config.toml
+    #curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/latest/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
 else
     ln -s -f ~/projects/dotfiles/.config/linux/* ~/.config/
     ln -s -f ~/projects/dotfiles/bin/linux/* ~/bin/
